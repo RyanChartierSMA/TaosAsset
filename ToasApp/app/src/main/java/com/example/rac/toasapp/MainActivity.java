99efb48d534b91479a8c1c12afddfd4f84930b64
RyanChartierSMA <Ryan.chartier@soudermiller.com>
2016-09-26 13:18:31 -0600

    Designed MainActivity

--- ToasApp/app/src/main/java/com/example/rac/toasapp/MainActivity.java
+++ ToasApp/app/src/main/java/com/example/rac/toasapp/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.rac.toasapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+//Create the buttons for Activity_main.xml
+
+    Button troubleShootbtn;
+    Button bt2;
+    Button bt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize buttons
+        troubleShootbtn = (Button)findViewById(R.id.trouble);
+        bt2 = (Button)findViewById(R.id.bt2);
+        bt3 = (Button)findViewById(R.id.bt3);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Button pressedButton = (Button)v;
+        switch(pressedButton.getText().toString()){
+
+            case "TroubleShoot":
+                //Open the next window - start new intent
+                break;
+
+        }
     }
 }
