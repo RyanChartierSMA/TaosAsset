d4fdd02a47972bfed576ef7b069eb8dd3e468771
RyanChartierSMA <Ryan.chartier@soudermiller.com>
2016-10-25 12:46:30 -0600

    Fixed Desription update and divide by zero error
    when there are no options and optionnum is 0, do not allow a divide by
    zero when assigning the weight per button. added base cases to handle
    that.

--- ToasApp/app/src/main/java/com/example/rac/toasapp/FoamFlowChart.java
+++ ToasApp/app/src/main/java/com/example/rac/toasapp/FoamFlowChart.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FoamFlowChart extends AppCompatActivity implements View.OnClickListener {
     /*the aim of this class is to populate a list based on the assets we have.
@@ -15,6 +16,7 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
     */
     //layout that will link to xml layout to add buttons
     LinearLayout buttonHolder;
+    TextView desc;
     int currentOptionNum = 4;
     int currentIndex = 0;
     Question[] masterChart;
@@ -22,14 +24,13 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
                                    //image, textview, and one space take up 30.
     float weightPerButton;//assigned during button creation
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asset_selection);
 
         buttonHolder = (LinearLayout)findViewById(R.id.buttonLayout);
-
+        desc = (TextView)findViewById(R.id.description);
         /*
         Button test = new Button(this);
         test.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT ));
@@ -82,11 +83,12 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
 
 //--------------------------------------------------------------Setup index 1
 
+        tempQ = new Question();
         tempQ.setQuestionText("Solids return from sludge processing. Try to reduce the solid return by improving the solids capture in sludge processing.");
 
-        tempQ = new Question();
         tempQ.setOp1(-2);
         tempQ.setButton1Text("");
+
         tempQ.setOp2(-2);
         tempQ.setOp2bImage(R.drawable.logo);
         tempQ.setButton2Text("");
@@ -94,7 +96,7 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
         tempQ.setButton3Text("");
         tempQ.setOp4(-2);
         tempQ.setButton4Text("");
-        tempQ.setOptionNums(4);
+        tempQ.setOptionNums(0);
         tempQ.setPrevious(0);//at the start
 
         tempQ.image = new ImageView(this);
@@ -114,84 +116,109 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
     //but for now it is going to draw the first question.
     //this function gets called after each option click
     //automated button creation by creating button 1 to i
-    private void drawButtons()
-    {
+    private void drawButtons() {
         String key = "op";
         currentOptionNum = masterChart[currentIndex].getOptionNums();
         //-----------------------layout stuff----------------------
-        weightPerButton = weightOfButtons/ currentOptionNum;
-        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0);
+        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0);
         //----------------------end layout stuff----------------------
 
 
+        if (currentOptionNum != 0) {
+            //assigns the buttons an equal weight, cumulative to 60.
+            for (int i = 1; i <= currentOptionNum; i++)//for all available options, start at op1
+            {
+                boolean enabledButton = true;//used for enable/disable button based on -2 index
+                ImageButton test = new ImageButton(this);
+                test.setAdjustViewBounds(true);//fixes the image scale bug
+                String tempName = key + i;
 
-        //assigns the buttons an equal weight, cumulative to 60.
-        for( int i = 1; i <= currentOptionNum; i++)//for all available options, start at op1
-        {
-            boolean enabledButton = true;//used for enable/disable button based on -2 index
-            ImageButton test = new ImageButton(this);
-            test.setAdjustViewBounds(true);//fixes the image scale bug
-            String tempName = key + i;
-
-            //assigns an id to the button depending on which button is created
-            //and assigns the text to the appropiate button text
+                //assigns an id to the button depending on which button is created
+                //and assigns the text to the appropiate button text
             /*
                 each question contains an image for op1 -opn buttons. They are loaded below, based
                 on which butotn it is. An id is assigned, 1 to n, for each button. Buttons are
                 destroyed after each click and reloaded, so the ids can be resused.
             */
 
-            switch (tempName)
-            {
-                case "op1":
-                    //test = masterChart[currentIndex].getOp1b();
-                    test.setImageResource(masterChart[currentIndex].getOp1Image());
-                    test.setId(1);
-                    //test.setText(masterChart[currentIndex].getButton1Text());
-                    if(masterChart[currentIndex].getOp1() == -2)
-                        enabledButton = false;
-                    break;
-                case "op2":
-                    test.setImageResource(masterChart[currentIndex].getOp2bImage());
-                    test.setId(2);
-                    //test.setText(masterChart[currentIndex].getButton2Text());
-                    break;
-                case "op3":
-                    test.setImageResource(masterChart[currentIndex].getOp3bImage());
-                    test.setId(3);
-                    //test.setText(masterChart[currentIndex].getButton3Text());
-                    break;
-                case "op4":
-                    test.setImageResource(masterChart[currentIndex].getOp4bImage());
-                    test.setId(4);
-                   // test.setText(masterChart[currentIndex].getButton4Text());
-                    break;
-                case "op5":
-                    test.setId(5);
-                    //test.setText(masterChart[currentIndex].getButton5Text());
-                    break;
-                case "op6":
-                    test.setId(6);
-                    //test.setText(masterChart[currentIndex].getButton6Text());
-                    break;
-                case "op7":
-                    test.setId(7);
-                    //test.setText(masterChart[currentIndex].getButton7Text());
-                    break;
-                case "op8":
-                    test.setId(8);
-                    //test.setText(masterChart[currentIndex].getButton8Text());
-                    break;
+                switch (tempName) {
+                    case "op1":
+                        //test = masterChart[currentIndex].getOp1b();
+                        test.setImageResource(masterChart[currentIndex].getOp1Image());
+                        test.setId(1);
+                        //test.setText(masterChart[currentIndex].getButton1Text());
+                        if (masterChart[currentIndex].getOp1() == -2)
+                            enabledButton = false;
+                        break;
+                    case "op2":
+                        test.setImageResource(masterChart[currentIndex].getOp2bImage());
+                        test.setId(2);
+                        //test.setText(masterChart[currentIndex].getButton2Text());
+                        break;
+                    case "op3":
+                        test.setImageResource(masterChart[currentIndex].getOp3bImage());
+                        test.setId(3);
+                        //test.setText(masterChart[currentIndex].getButton3Text());
+                        break;
+                    case "op4":
+                        test.setImageResource(masterChart[currentIndex].getOp4bImage());
+                        test.setId(4);
+                        // test.setText(masterChart[currentIndex].getButton4Text());
+                        break;
+                    case "op5":
+                        test.setImageResource(masterChart[currentIndex].getOp5bImage());
+                        test.setId(5);
+                        //test.setText(masterChart[currentIndex].getButton5Text());
+                        break;
+                    case "op6":
+                        test.setImageResource(masterChart[currentIndex].getOp6bImage());
+                        test.setId(6);
+                        //test.setText(masterChart[currentIndex].getButton6Text());
+                        break;
+                    case "op7":
+                        test.setImageResource(masterChart[currentIndex].getOp7bImage());
+                        test.setId(7);
+                        //test.setText(masterChart[currentIndex].getButton7Text());
+                        break;
+                    case "op8":
+                        test.setId(8);
+                        test.setImageResource(masterChart[currentIndex].getOp8bImage());
+                        //test.setText(masterChart[currentIndex].getButton8Text());
+                        break;
+                }
+                test.setOnClickListener(this);
+                //all the neccessary layout-adding stuff. It adds the button to the layout.
+                if(currentOptionNum == 0)
+                {
+                    weightPerButton = 15;
+                }
+                else
+                {
+                    weightPerButton = weightOfButtons/currentOptionNum;
+                }
+                p.weight = weightPerButton;
+                test.setScaleType(ImageView.ScaleType.FIT_XY);
+                test.setLayoutParams(p);
+                test.setEnabled(enabledButton);
+                buttonHolder.addView(test);
             }
 
-            test.setOnClickListener(this);
-            //all the neccessary layout-adding stuff. It adds the button to the layout.
-            p.weight = weightPerButton;
+        }
+        else { //there are no buttons to draw, this is the end of the chart. Create a restart button.
+
+            ImageButton test = new ImageButton(this);
+            test.setAdjustViewBounds(true);//fixes the image scale bug
             test.setScaleType(ImageView.ScaleType.FIT_XY);
             test.setLayoutParams(p);
-            test.setEnabled(enabledButton);
+            test.setImageResource(R.drawable.restartbutton);
             buttonHolder.addView(test);
         }
+
+        updateDesc(currentIndex);
+    }
+    private void updateDesc(int index)
+    {
+        desc.setText(masterChart[index].getQuestionText());
     }
 
     @Override
@@ -211,7 +238,6 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
                 currentIndex = masterChart[currentIndex].getOp1();
                 removeButtons();
                 currentOptionNum = masterChart[currentIndex].getOptionNums();
-                weightPerButton = weightOfButtons/currentOptionNum;
                 drawButtons();//reloads the screen
                 break;
 
@@ -219,7 +245,6 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
                 currentIndex = masterChart[currentIndex].getOp2();
                 removeButtons();
                 currentOptionNum = masterChart[currentIndex].getOptionNums();
-                weightPerButton = weightOfButtons/currentOptionNum;
                 drawButtons();//reloads the screen
                 break;
 
@@ -227,14 +252,12 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
                 currentIndex = masterChart[currentIndex].getOp3();
                 removeButtons();
                 currentOptionNum = masterChart[currentIndex].getOptionNums();
-                weightPerButton = weightOfButtons/currentOptionNum;
                 drawButtons();//reloads the screen
                 break;
             case 4:
                 currentIndex = masterChart[currentIndex].getOp4();
                 removeButtons();
                 currentOptionNum = masterChart[currentIndex].getOptionNums();
-                weightPerButton = weightOfButtons/currentOptionNum;
                 drawButtons();//reloads the screen
                 break;
 
@@ -242,7 +265,6 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
                 currentIndex = masterChart[currentIndex].getOp5();
                 removeButtons();
                 currentOptionNum = masterChart[currentIndex].getOptionNums();
-                weightPerButton = weightOfButtons/currentOptionNum;
                 drawButtons();//reloads the screen
                 break;
 
@@ -250,7 +272,6 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
                 currentIndex = masterChart[currentIndex].getOp6();
                 removeButtons();
                 currentOptionNum = masterChart[currentIndex].getOptionNums();
-                weightPerButton = weightOfButtons/currentOptionNum;
                 drawButtons();//reloads the screen
                 break;
 
@@ -258,7 +279,6 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
                 currentIndex = masterChart[currentIndex].getOp7();
                 removeButtons();
                 currentOptionNum = masterChart[currentIndex].getOptionNums();
-                weightPerButton = weightOfButtons/currentOptionNum;
                 drawButtons();//reloads the screen
                 break;
 
@@ -266,7 +286,6 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
                 currentIndex = masterChart[currentIndex].getOp8();
                 removeButtons();
                 currentOptionNum = masterChart[currentIndex].getOptionNums();
-                weightPerButton = weightOfButtons/currentOptionNum;
                 drawButtons();//reloads the screen
                 break;
         }
@@ -282,7 +301,6 @@ public class FoamFlowChart extends AppCompatActivity implements View.OnClickList
 
         }
     }
-
 
 
 }
